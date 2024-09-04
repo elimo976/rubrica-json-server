066a8b08b367ebd87d76b2e9b70d50d834454422
username / Elisa + Morreale <elisamorreale@gmail.com>
2024-09-04 16:58:42 +0200

    creazione rubrica con json server integrato

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormContattoComponent } from './components/form-contatto/form-contatto.component';
+import { ContattiListComponent } from './components/contatti-list/contatti-list.component';
+import { ContattoDetailComponent } from './components/contatto-detail/contatto-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ContattiListComponent },
+  { path: 'contatti/:id', component: ContattoDetailComponent },
+  { path: 'modifica/:id', component: FormContattoComponent }, // Percorso per la modifica
+  { path: 'add', component: FormContattoComponent } // Percorso per l'aggiunta di un nuovo contatto
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormContattoComponent } from './components/form-contatto/form-contatto.component';
+import { ContattiListComponent } from './components/contatti-list/contatti-list.component';
+import { ContattoDetailComponent } from './components/contatto-detail/contatto-detail.component';
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+registerLocaleData(localeIt);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormContattoComponent,
+    ContattiListComponent,
+    ContattoDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'it-IT'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
